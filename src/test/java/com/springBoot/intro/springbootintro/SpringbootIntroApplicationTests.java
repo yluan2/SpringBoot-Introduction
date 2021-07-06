cda8a06358459194daae8fea2380fa0e98e9be64
yluan2 <luanyx1126@gmail.com>
2021-07-06 19:00:52 -0400

    First Commit

---
+++ src/test/java/com/springBoot/intro/springbootintro/SpringbootIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.intro.springbootintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
