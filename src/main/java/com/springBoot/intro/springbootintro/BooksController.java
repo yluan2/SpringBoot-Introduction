3faa8a8bc7539b986ee02059faa5b2bb083fedcb
yluan2 <luanyx1126@gmail.com>
2021-07-08 15:50:34 -0400

    Add Book Controller

---
+++ src/main/java/com/springBoot/intro/springbootintro/BooksController.java
@@ -0,0 +1,18 @@
+package com.springBoot.intro.springbootintro;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class BooksController {
+	@GetMapping("/books")
+	
+	public List<Book> getAllBooks(){
+		return Arrays.asList(
+				new Book(1l, "Master A", "author A"));
+	}
+}
