016a38824ff5360f6be1d875353671660576559a
yluan2 <luanyx1126@gmail.com>
2021-07-08 15:49:30 -0400

    Add BookBean

--- src/main/java/com/springBoot/intro/springbootintro/SpringbootIntroApplication.java
+++ src/main/java/com/springBoot/intro/springbootintro/SpringbootIntroApplication.java
@@ -1,13 +1,25 @@
 package com.springBoot.intro.springbootintro;
 
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+//spring context
+//enable auto configuration
+//Component scan -> important feature
 public class SpringbootIntroApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIntroApplication.class, args);
+		ConfigurableApplicationContext applicationContext =  
+				SpringApplication.run(SpringbootIntroApplication.class, args);
+		//SpringbootIntroApplication  -> Spring context
+		
+		for(String name: applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
